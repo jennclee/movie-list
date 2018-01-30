07ee948f1ec0ba05c7b042c0989f284686a2b1f1
jennclee <8753352+jennclee@users.noreply.github.com>
2018-01-30 14:44:50 -0800

    Init commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # movie-list
+
+## Description
+A simple movie list application to store your favorite movies and movies that you want to watch
+
+## Technologies
+- React 16.2.x
+- Webpack 3.10.x
+- Node 6.9.x
+- Express 4.16.x
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+const config = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
+
+module.exports = config
