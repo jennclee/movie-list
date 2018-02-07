8dbed6c248297498329f35b18bd965ac51ac2765
jennclee <8753352+jennclee@users.noreply.github.com>
2018-02-06 16:35:42 -0800

    Add Search component to app

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Movie from './components/Movie.jsx';
+import Search from './components/Search.jsx';
 
 class MovieList extends React.Component {
   constructor() {
@@ -13,12 +14,22 @@ class MovieList extends React.Component {
         { title: 'Sunshine' },
         { title: 'Ex Machina' },
       ],
+      searchText: '',
     };
+  }
+
+  search(text) {
+    this.setState({
+      searchText: text
+    });
   }
 
   render() {
     return (
       <div>
+        <div className="navbar">
+          <Search search={this.search} />
+        </div>
         {this.state.movies.map((movie) => {
           return <Movie movie={movie} key={movie.title} />;
         })}
