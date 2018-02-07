4f18549e54147094d216aa075a74801f1a6bf232
jennclee <8753352+jennclee@users.noreply.github.com>
2018-02-06 16:36:06 -0800

    Create basic Search component

---
+++ client/src/components/Search.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+    }
+    this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleOnSubmit = this.handleOnSubmit.bind(this);
+  }
+
+  handleOnChange(event) {
+    this.setState({
+      searchText: event.target.value
+    });
+  }
+  
+  handleOnSubmit() {
+    this.props.search(this.state.searchText);
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input onChange={this.handleOnChange} type="text" />
+          <button onClick={this.handleOnSubmit} type="submit">Search</button>
+        </form>
+      </div>
+    );
+  }
+};
