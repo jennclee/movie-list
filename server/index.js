241f7ec3232c62f92d6b3650754e304195d9ed11
jennclee <8753352+jennclee@users.noreply.github.com>
2018-02-14 10:32:19 -0800

    Implement API routes for retrieving and adding movies

--- server/index.js
+++ server/index.js
@@ -7,3 +7,24 @@ const path = require('path');
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.listen(3000, function () { console.log('MovieList app listening on port 3000!') });
+
+const movies = [
+  { title: 'Mean Girls', watched: false },
+  { title: 'Hackers', watched: false },
+  { title: 'The Grey', watched: false },
+  { title: 'Sunshine', watched: false },
+  { title: 'Ex Machina', watched: false },
+];
+
+app.get('/movies', (req, res) => {
+  res.send(movies);
+});
+
+app.post('/movie', (req, res) => {
+  const newMovieObj = {
+    title: req.body.newMovie,
+    watched: false
+  };
+  movies.push(newMovieObj);
+  res.status(200).send('POST successful!');
+});
